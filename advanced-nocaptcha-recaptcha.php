1cd9b4ce60ef4d0ff3e09548fa70387d8cbb66c7
Shamim Hasan <shamim_biplob@yahoo.com>
2018-08-12 13:13:25 +0600

    follow coding standard

--- advanced-nocaptcha-recaptcha.php
+++ advanced-nocaptcha-recaptcha.php
@@ -9,17 +9,15 @@ Author URI: https://www.shamimsplugins.com/contact-us/
 Text Domain: advanced-nocaptcha-recaptcha
 License: GPLv2 or later
 */
-//DEFINE
-define('ANR_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
-define('ANR_PLUGIN_URL', plugins_url( '/', __FILE__ ) );
+// DEFINE
+define( 'ANR_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'ANR_PLUGIN_URL', plugins_url( '/', __FILE__ ) );
 
-require_once('functions.php');
+require_once 'functions.php';
 
+// ADD ACTIONS
+add_action( 'after_setup_theme', 'anr_include_require_files' );
+add_action( 'plugins_loaded', 'anr_translation' );
+// add_action( 'wp_enqueue_scripts', 'anr_enqueue_scripts' );
+add_action( 'login_enqueue_scripts', 'anr_login_enqueue_scripts' );
 
-	//ADD ACTIONS
-	
-	add_action('after_setup_theme', 'anr_include_require_files');
-	add_action('plugins_loaded', 'anr_translation');
-	//add_action('wp_enqueue_scripts', 'anr_enqueue_scripts');
-	add_action('login_enqueue_scripts', 'anr_login_enqueue_scripts');
-	
