19f7547b64e43e3a79a9b4086dcdcb93778e09c3
Shamim Hasan <shamim_biplob@yahoo.com>
2019-04-16 12:11:45 +0600

    ANR class added

--- advanced-nocaptcha-recaptcha.php
+++ advanced-nocaptcha-recaptcha.php
@@ -9,16 +9,52 @@ Author URI: https://www.shamimsplugins.com/contact-us/
 Text Domain: advanced-nocaptcha-recaptcha
 License: GPLv2 or later
 */
-// DEFINE
-define( 'ANR_PLUGIN_VERSION', '4.3' );
-define( 'ANR_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'ANR_PLUGIN_URL', plugins_url( '/', __FILE__ ) );
 
-require_once 'functions.php';
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
 
-// ADD ACTIONS
-add_action( 'after_setup_theme', 'anr_include_require_files' );
-add_action( 'plugins_loaded', 'anr_translation' );
-// add_action( 'wp_enqueue_scripts', 'anr_enqueue_scripts' );
-add_action( 'login_enqueue_scripts', 'anr_login_enqueue_scripts' );
+class ANR {
 
+	private static $instance;
+
+	private function __construct() {
+		if ( function_exists( 'anr_get_option' ) ) {
+			if ( ! function_exists( 'deactivate_plugins' ) ) {
+				require_once ABSPATH . 'wp-admin/includes/plugin.php';
+			}
+			deactivate_plugins( 'advanced-nocaptcha-recaptcha/advanced-nocaptcha-recaptcha.php' );
+			return;
+		}
+		$this->constants();
+		$this->includes();
+		$this->actions();
+		// $this->filters();
+	}
+
+	public static function init() {
+		if ( ! self::$instance instanceof self ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	private function constants() {
+		define( 'ANR_PLUGIN_VERSION', '4.4' );
+		define( 'ANR_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+		define( 'ANR_PLUGIN_URL', plugins_url( '/', __FILE__ ) );
+		define( 'ANR_PLUGIN_FILE', __FILE__ );
+	}
+
+	private function includes() {
+		require_once ANR_PLUGIN_DIR . 'functions.php';
+	}
+
+	private function actions() {
+		add_action( 'after_setup_theme', 'anr_include_require_files' );
+		add_action( 'init', 'anr_translation' );
+		add_action( 'login_enqueue_scripts', 'anr_login_enqueue_scripts' );
+	}
+} //END Class
+
+ANR::init();
