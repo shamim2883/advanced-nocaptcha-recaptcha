f4356d0831efce65f8081a71e0713d1ceeaf9c5d
Shamim Hasan <shamim_biplob@yahoo.com>
2019-06-20 10:41:50 +0600

    return last verify incase of duplicate checking

--- functions.php
+++ functions.php
@@ -189,6 +189,8 @@ function anr_captcha_form_field( $echo = false ) {
 }
 
 function anr_verify_captcha( $response = false ) {
+	static $last_verify = null;
+	
 	$secre_key  = trim( anr_get_option( 'secret_key' ) );
 	$remoteip = $_SERVER['REMOTE_ADDR'];
 	$verify = false;
@@ -209,6 +211,10 @@ function anr_verify_captcha( $response = false ) {
 
 	if ( ! $response || ! $remoteip ) {
 		return $verify;
+	}
+	
+	if ( null !== $last_verify ) {
+		return $last_verify;
 	}
 
 	$url = 'https://www.google.com/recaptcha/api/siteverify';
@@ -240,8 +246,10 @@ function anr_verify_captcha( $response = false ) {
 			$verify = true;
 		}
 	}
+	$verify = apply_filters( 'anr_verify_captcha', $verify, $result, $response );
+	$last_verify = $verify;
 
-	return apply_filters( 'anr_verify_captcha', $verify, $result, $response );
+	return $verify;
 }
 
 add_filter( 'shake_error_codes', 'anr_add_shake_error_codes' );
